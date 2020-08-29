c99fe60048e7019c21c6fdc225c4c90efe2628a7
vikas <vikas@tradala.in>
2020-08-29 17:24:27 +0530

    understanding of safeareaview and text properties

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello!</Text>
+      <Text numberOfLines={1}>Hello, this is longer size text to check what happens  when we add long text !</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
