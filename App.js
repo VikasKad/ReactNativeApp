1be1737a14263e05bd8c0c240ec9c4a82823857e
vikas <vikas@tradala.in>
2020-08-29 19:58:12 +0530

    color change only

--- App.js
+++ App.js
@@ -6,25 +6,17 @@ export default function App() {
   return (
     <View
       style={{
-        backgroundColor: "dodgerblue",
+        backgroundColor: "#fff",
         flex: 1,
         flexDirection: "row",
         justifyContent: "center",
-        alignItems:'center',
+        alignItems: "center",
       }}
     >
       <View style={{ backgroundColor: "red", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "green", width: 100, height: 100 }} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
