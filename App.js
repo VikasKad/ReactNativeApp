ddc2ee02929eafa84157c8f213e7aaa80b640011
vikas <vikas@tradala.in>
2020-08-29 19:55:24 +0530

    with flex directions and its other properties

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}>
+    <View
+      style={{
+        backgroundColor: "dodgerblue",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems:'center',
+      }}
+    >
       <View style={{ backgroundColor: "red", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
