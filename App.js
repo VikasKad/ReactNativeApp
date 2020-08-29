132e422bf39077d0767aaf27ebcc2de3194f8032
vikas <vikas@tradala.in>
2020-08-29 20:01:52 +0530

    flexbasic and flex use

--- App.js
+++ App.js
@@ -15,10 +15,18 @@ export default function App() {
         flexWrap: "wrap",
       }}
     >
-      <View style={{ backgroundColor: "red", width: 100, height: 100 }} />
+      <View
+        style={{
+          backgroundColor: "blue",
+          width: 100,
+          height: 100,
+          flex: 1,
+          flexBasis: 100,
+        }}
+      />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "green", width: 100, height: 100 }} />
+      {/* <View style={{ backgroundColor: "green", width: 100, height: 100 }} /> */}
     </View>
   );
 }
